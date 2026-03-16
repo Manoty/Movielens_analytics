c397d954aacafa1b9d7dcd729fbc99d572480d6e
Kevin Manoti <kevinnoty21@gmail.com>
2026-03-16 14:59:46 +0300

    add movie rating model

---
+++ models/intermediate/int_movie_ratings.sql
@@ -0,0 +1,8 @@
+SELECT
+    movie_id,
+    COUNT(*) AS total_ratings,
+    AVG(rating) AS avg_rating,
+    MIN(rating_timestamp) AS first_rating,
+    MAX(rating_timestamp) AS last_rating
+FROM {{ ref('stg_ratings') }}
+GROUP BY movie_id
